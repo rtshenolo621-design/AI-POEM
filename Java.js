59627ce7e8fceb1aa82bb745d369ffbe49b7d974
Tsheno-Codes-design <rtshenolo621@gmail.com>
2026-02-20 15:53:07 +0200

    Added basic form handling & Typewriter plugin

--- Java.js
+++ Java.js
@@ -1,1 +1,13 @@
-alert("Its working!!");
+function generatePoem(event) {
+    event.preventDefault();
+
+    new Typewriter("#poem", {
+      strings: ["Hold fast to dreams"],
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
